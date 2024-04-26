00f02d2abe1b30ed4fe8d6f062eede05df757c59
Alejandro Bordallo <alex.bordallo@gmail.com>
2024-04-26 14:07:16 +0100

    Update docker scripts to use `:latest` tag

--- dev.sh
+++ dev.sh
@@ -5,7 +5,7 @@
 
 # Build docker image up to dev stage
 DOCKER_BUILDKIT=1 docker build \
-    -t av_bridge_noetic_galactic \
+    -t av_ros1_bridge:latest \
     -f Dockerfile --target dev .
 
 # Run docker image with local code volumes for development
@@ -22,4 +22,4 @@ docker run -it --rm --net host --privileged \
     -v ./test:/opt/ros1_bridge_ws/src/ros1_bridge/test \
     -v ./CMakeLists.txt:/opt/ros1_bridge_ws/src/ros1_bridge/CMakeLists.txt \
     -v ./package.xml:/opt/ros1_bridge_ws/src/ros1_bridge/package.xml \
-    av_bridge_noetic_galactic
+    av_ros1_bridge:latest
--- runtime.sh
+++ runtime.sh
@@ -13,10 +13,10 @@ fi
 
 # Build docker image only up to base stage
 DOCKER_BUILDKIT=1 docker build \
-    -t av_bridge_noetic_galactic \
+    -t av_ros1_bridge:latest \
     -f Dockerfile --target runtime .
 
 # Run docker image without volumes
 docker run -it --rm --net host --privileged \
     -v /dev/shm:/dev/shm \
-    av_bridge_noetic_galactic $CMD
+    av_ros1_bridge:latest $CMD
